821d3f1bdbeac89b5a6abc5476a08d743833017b
alexmmych <alexmmych@gmail.com>
2024-08-01 18:56:06 +0200

    Renamed all files according to new project name

--- android/app/src/main/kotlin/com/example/flutter_application/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_application/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_application
+package com.example.universal_organizer
 
 import io.flutter.embedding.android.FlutterActivity
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_application/main.dart';
+import 'package:universal_organizer/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
