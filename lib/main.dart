947fa2757e96309b809c598fcf99e237505e90c8
alexmmych <alexmmych@gmail.com>
2024-08-06 11:48:05 +0200

    Adding basic AppBar functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,82 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool useMaterial3 = true;
+  ThemeMode themeMode = ThemeMode.system;
+  Color colorSelected = Colors.blue;
+
+  Color contrastColor = Colors.white;
+  Color contrastColorLight = Colors.white54;
+
+  Color oppositeColor = Colors.black;
+  Color oppositeColorLight = Colors.black54;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World! TEST'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: useMaterial3,
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: colorSelected, brightness: Brightness.dark),
       ),
+      home: TopBar(
+        contrastColor: contrastColor,
+        colorSelected: colorSelected,
+        contrastColorLight: contrastColorLight,
+        oppositeColor: oppositeColor,
+        oppositeColorLight: oppositeColorLight,
+      ),
+      themeMode: themeMode,
     );
+  }
+}
+
+class TopBar extends StatelessWidget {
+  const TopBar({
+    super.key,
+    required this.contrastColor,
+    required this.colorSelected,
+    required this.contrastColorLight,
+    required this.oppositeColor,
+    required this.oppositeColorLight,
+  });
+
+  final Color contrastColor;
+  final Color colorSelected;
+  final Color contrastColorLight;
+  final Color oppositeColor;
+  final Color oppositeColorLight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      backgroundColor: contrastColorLight,
+      title: const Text("Test"),
+      actions: <Widget>[
+        FloatingActionButton(
+          shape: const CircleBorder(),
+          backgroundColor: contrastColor,
+          hoverColor: colorSelected,
+          splashColor: contrastColorLight,
+          child: Icon(CupertinoIcons.moon,
+              color: oppositeColor, size: 24.0, semanticLabel: "Night Mode"),
+          onPressed: () => print("Button pressed"),
+        ),
+      ],
+    ));
   }
 }
--- test/basic_test.dart
+++ test/basic_test.dart
@@ -4,10 +4,10 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets("Widget contains 'Hello World!' text", (tester) async {
     // Create the widget by telling the tester to build it.
-    await tester.pumpWidget(const MainApp());
+    await tester.pumpWidget(const App());
 
     // Create the Finders.
-    final messageFinder = find.text('Hello World! TEST');
+    final messageFinder = find.text('Hello World!');
 
     // Use the `findsOneWidget` matcher provided by flutter_test to verify
     // that the Text widgets appear exactly once in the widget tree.
